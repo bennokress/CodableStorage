c4aa221cd35ca7fee97bbc3e7d59d43bb855996a
BK | Mobile Apps <developer+git-commit@bennokress.de>
2023-09-27 15:00:36 +0200

    Update Swift Version and License

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,10 +8,10 @@ private let codableStorage = "CodableStorage"
 let package = Package(
     name: codableStorage,
     platforms: [
-        .iOS(.v11),
+        .iOS(.v12),
         .macCatalyst(.v13),
         .macOS(.v10_13),
-        .tvOS(.v11),
+        .tvOS(.v12),
         .watchOS(.v4)
     ],
     products: [
